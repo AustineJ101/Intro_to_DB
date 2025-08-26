f74295484fd300b95bca07d6d2c1dac1e787a106
Austine Juma <austinejumah001@gmail.com>
2025-08-26 15:49:51 +0300

    Add IF NOT EXISTS to CREATE DATABASE

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,19 +1,19 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
+
+CREATE TABLE Authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
 
 CREATE TABLE Books(
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
-    publication_date DATE
+    publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
-);
-
-CREATE TABLE Authors(
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
 );
 
 CREATE TABLE Customers(
